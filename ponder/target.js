4427b5febf472387e4d5f4756363bb810d9b0bfe
MushuMan <112738395+MushuMan@users.noreply.github.com>
2025-05-01 10:53:15 -0600

    did

---
+++ ponder/target.js
@@ -0,0 +1,7 @@
+const intro = document.querySelector('#intro');
+intro.style.backgroundColor = '#f2c556';
+const em = document.querySelector('em');
+em.textContent = 'USS Voyager Starship';
+
+const starshipImg = document.querySelector('#starship');
+starshipImg.setAttribute('src', 'https://bit.ly/');
